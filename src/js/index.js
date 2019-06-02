95d3a62af8d162b6e45486274693b169092d5967
arty <feelgrime@mail.ru>
2019-06-02 12:58:59 +0300

    im done page search.html

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,21 @@ import $ from 'jquery';
 import '../less/style.less';
 import '../css/style.css';
 import '../index.html';
+import '../search.html';
+import '../roomDetalis.html';
+import '../lib/moment.js'
+import '../lib/moment-with-locales.min.js';
+import '../lib/JqueryUI/jquery-ui.js';
+
+import '../lib/bootstrapDatepicker/bootstrap-datepicker.js'
+import '../lib/bootstrapDatepicker/bootstrap-datepicker.ru.min.js';
+import '../lib/bootstrapDatepicker/bootstrap-datepicker3.css';
+import '../lib/bootstrapDatepicker/bootstrap-datepicker3.standalone.css';
+
+import '../lib/JqueryUI/jquery-ui.css';
+import '../lib/JqueryUI/jquery-ui.theme.css';
+
+
 
 $(document).ready(function(){
 	$('.logo').click(function(){
@@ -20,7 +35,133 @@ $(document).ready(function(){
 		console.log(kek);
 		kek.change();
 	});
-	$(window).dblclick(function(e){
-	})
+
+
+	moment().format('YYYY MM DD');
+	$('.calendarBootstrap').datepicker({
+	});
+
+
+	$('.input-wrapper').click(function(event){
+		$('.calendarBootstrap').toggleClass('on');
+		$('.calendarBootstrap').slideToggle('on');
+	});
+	$('.form__button').hover(function()
+		{
+			$('.arrow-3-iconcircle').css({"stroke-dashoffset":"0"});
+			$('.arrow-3-icon').css('transform','translate3d(5px, 0, 0)');
+		},
+		function() {$('.arrow-3-iconcircle').css({"stroke-dashoffset":""});
+			$('.arrow-3-icon').css('transform','translate3d(0, 0, 0)');}
+		);
+
+
+		$('.select-wrapper').click(function(){
+			//$('.select-wrapper > select > option').remove();
+			$(this).parent().children('.dropdown').slideToggle();
+		});
+
+		$('.plus').click(function(){
+			let check = $(this).parent().children('span').text();
+			$(this).parent().children('span').text(++check);
+		});
+		$('.min').click(function(){
+			if($(this).parent().children('span').text() == 0) {
+				$(this).parent().children('span').text('0');
+			}
+			else {
+				let check = $(this).parent().children('span').text();
+				$(this).parent().children('span').text(--check);
+			}
+		});
+
+		$('.dropdown .clear').click(function(){
+			$('.wrapper-count > .count').text('0');
+			$('.select-wrapper > select > option').text("Сколько гостей");
+		});
+
+		$('.dropdown .apply').click(function(){
+			let sumGuests = [];
+			let one = $('.wrapper-count > .count').text();
+			for(let i=0;i<one.length;i++){
+				let push = $('.wrapper-count > .count').eq(i).text();
+				sumGuests.push(push);
+				var summElem = sumGuests.reduce(function(sum,curr){
+					return parseInt(sum+parseInt(curr));
+				},0); 
+			}
+			$(this).parent('.select-wrapper').children('.select-wrapper').children('select').children('option').text(summElem + " Гостя(Гостей)");
+		});
+		$('.form__button').click(function(e){
+			e.preventDefault();
+			window.location.href = ("http://localhost:8080/src/search.html");
+		});
+
+
+		let add = 0;
+		let arr = [];
+		$('.comfortsSelect .plus').click(function(){
+			let text = $(this).parent().parent().children('span').text();
+			let num = $(this).parent().children('.count').text();
+			let length = $('.comfortsSelect__dropdown').children().length;
+			let summ = [];
+			for(let i=0;i<length;i++) {
+				let text1 = $('.comfortsSelect__dropdown').children().eq(i).children('span').text();
+				let text2 = $('.comfortsSelect__dropdown').children().eq(i).children('.wrapper-count').children('.count').text();
+				summ += text2 + " " + text1 + ", ";
+			}	
+			$('.comfortsSelect > .select-wrapper > select>option').text(summ);
+		});
+
+		$('.comfortsSelect .min').click(function(){
+			let text = $(this).parent().parent().children('span').text();
+			let num = $(this).parent().children('.count').text();
+			let length = $('.comfortsSelect__dropdown').children().length;
+			let summ = [];
+			for(let i=0;i<length;i++) {
+				let text1 = $('.comfortsSelect__dropdown').children().eq(i).children('span').text();
+				let text2 = $('.comfortsSelect__dropdown').children().eq(i).children('.wrapper-count').children('.count').text();
+				summ += text2 + " " + text1 + ", ";
+			}	
+			$('.comfortsSelect > .select-wrapper > select>option').text(summ);
+		});
+
+		$('.aditionals__select-wrapper').click(function(){
+			$('.dropdownAdditionals').slideToggle();
+		});
+		$('.breadCrumb > div >span>a').click(function(e){
+			e.preventDefault();
+			let length = $('.breadCrumb > div > span').length;
+			console.log(length);
+			for(let i=0;i<length;i++) {
+				$('.breadCrumb>div>span').eq(i).children('a').removeClass('breadCrumb__active');
+			}
+			$(this).toggleClass('breadCrumb__active');
+		});
+
+		$('.hotels__table').click(function(){
+			window.location.href = "roomDetalis.html";
+		});
 });
+
+
+
+
+$(function(){
+ 
+	 $( "#slider-range" ).slider({
+		 range: true,
+		 min: 0,
+		 max: 15000,
+		 values: [ 4000, 8500 ],
+		 step: 100,
+		 slide: function( event, ui ) {
+		 $( ".rangePrices > div").eq(0).children('span').text(ui.values[ 0 ] + "₽ - " + ui.values[ 1 ] + "₽");
+		 }
+		 });
+		 $( ".rangePrices > div").eq(0).children('span').text($( "#slider-range" ).slider( "values", 0 ) +
+		 "₽  - " + $( "#slider-range" ).slider( "values", 1 ) + "₽" );
+});
+
+
 
